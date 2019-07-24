1e97059acc31f684cdabd0d835a0d98240b36be3
MadVodka <ivanshblu@gmail.com>
2019-07-24 09:45:23 +0400

    Edited .gitignore

--- ReadingFile/src/com/epam/file/exceptions/MyReaderException.java
+++ ReadingFile/src/com/epam/file/exceptions/MyReaderException.java
@@ -1,8 +1,6 @@
 package com.epam.file.exceptions;
 
-import java.io.IOException;
-
-public class MyReaderException extends IOException {
+public class MyReaderException extends Exception {
     public MyReaderException(String message) {
         super(message);
     }
